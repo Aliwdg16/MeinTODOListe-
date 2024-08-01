a71e83f4b2daab7d5ed1582d770a8870544d392d
aliwdg16 <alialtoto@hotmail.com>
2024-08-01 13:49:05 +0200

    deploy2

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use(cors({ origin:['https://todolisthassan.netlify.app','http://localhost:5173','https://todoliste.onrender.com'] ,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    // methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true }));
 
 //routes
